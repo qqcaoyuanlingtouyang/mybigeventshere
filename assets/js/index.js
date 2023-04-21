2739ff0911d4bfc54873e1645753ec2d61ffc007
liufengjuan <3214277091@qq.com>
2023-04-21 20:17:08 +0800

    完成了首页制作

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+$(function () {
+    getUserInfo()
+    let layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'GET',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            console.log(res);
+            renderAvatar(res.data)
+        },
+        // complete: function (res) {
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         localStorage.removeItem('token')
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+function renderAvatar(user) {
+    let name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        let first = name[0].toUpperCase()
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(first).show()
+    }
+}
